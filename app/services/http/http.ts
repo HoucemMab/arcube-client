accdfde8e66ba0db7e42d0335257d1f6a0e17983
HoucemUnty <houssam@unty.fr>
2025-02-01 13:35:30 +0100

    backend basurl

--- app/services/http/http.ts
+++ app/services/http/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosClient = () => {
   const client = axios.create({
     //todo add this to env file
-    baseURL: `http://localhost:3005/`,
+    baseURL: `https://arcube-api.netlify.app/`,
     timeout: 60000,
     withCredentials: true,
   });
