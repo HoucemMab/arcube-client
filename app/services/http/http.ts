193d5b83cfa4ac128aa3f893cc3a3dad43922240
HoucemUnty <houssam@unty.fr>
2025-02-01 13:53:38 +0100

    add backend

--- app/services/http/http.ts
+++ app/services/http/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosClient = () => {
   const client = axios.create({
     //todo add this to env file
-    baseURL: `https://arcube-api.netlify.app/`,
+    baseURL: `https://arcube-server-m5u0a77vh-houcemmabs-projects.vercel.app/`,
     timeout: 60000,
     withCredentials: true,
   });
