a12212d60679dcf190ac39f73ac288ba336be17d
HoucemUnty <houssam@unty.fr>
2025-02-01 14:11:39 +0100

    edit url

--- app/services/http/http.ts
+++ app/services/http/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosClient = () => {
   const client = axios.create({
     //todo add this to env file
-    baseURL: `https://arcube-server-856xebs2c-houcemmabs-projects.vercel.app`,
+    baseURL: `https://arcube-server-jngepf8io-houcemmabs-projects.vercel.app`,
     timeout: 60000,
     withCredentials: true,
   });
